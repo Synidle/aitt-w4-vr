1fc4fa935b688e914f036af7990ca19763bb6b75
Ben Lockyer <73420926+Synidle@users.noreply.github.com>
2026-03-03 17:50:06 +0000

    somewhat

--- Assets/OffroadTeleport.cs
+++ Assets/OffroadTeleport.cs
@@ -5,35 +5,38 @@ using UnityEngine;
 using Valve.VR;
 
 public class OffroadTeleport : MonoBehaviour
-{    
+{
+    [SerializeField] Transform raycastBasis; 
+
     private Vector3 hitPoint;
     private LineRenderer lineRenderer;
+    private float height;
 
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        //height = transform.position.y; 
+    }
+
+    private void FixedUpdate()
+    {
+        Ray raycast = new Ray(raycastBasis.position, raycastBasis.forward);
+        RaycastHit hit;
+
+        bool bHit = Physics.Raycast(raycast, out hit);
+        if (bHit)
+        {
+            hitPoint = hit.point;
+
+            lineRenderer.SetPosition(0, raycastBasis.position);
+            lineRenderer.SetPosition(1, hitPoint);
+        }
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (SteamVR_Input.GetStateDown("OffroadTeleport", SteamVR_Input_Sources.LeftHand))
-        {
-            Ray raycast = new Ray(transform.position, transform.forward);
-            RaycastHit hit;
-
-            print("Teleport input down");
-
-            bool bHit = Physics.Raycast(raycast, out hit);
-            if (bHit)
-            {
-                hitPoint = hit.point;
-
-                lineRenderer.SetPosition(0, transform.position);
-                lineRenderer.SetPosition(1, hitPoint);
-            }
-        }
+    { 
 
         if (SteamVR_Input.GetStateUp("OffroadTeleport", SteamVR_Input_Sources.LeftHand))
         {
@@ -46,6 +49,6 @@ public class OffroadTeleport : MonoBehaviour
     private void TeleportTo(Vector3 position)
     {
         transform.position = new Vector3(
-            position.x, transform.position.y, position.z);
+            position.x, position.y+height, position.z);
     }
 }
